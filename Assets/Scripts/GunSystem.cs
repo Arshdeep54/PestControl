f7bf4cfd1b27af2df8f21721ac397ac6d7cdb2dd
Arshdeep54 <balarsh535@gmail.com>
2024-07-20 14:40:44 +0530

    bullet+E

---
+++ Assets/Scripts/GunSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GunSystem : MonoBehaviour
+{
+
+    public Transform BulletSpawnPoint;
+    public GameObject Bullet;
+
+    public float speed = 1f;
+    void Update()   
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            var bullet = Instantiate(Bullet, BulletSpawnPoint.position, BulletSpawnPoint.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = BulletSpawnPoint.forward * speed;
+
+        }
+
+    }
+
+
+}
